490b15374a97bcbb5cdabab934ba506d0781067f
LambdaExpression <lambdaexpressio@139.com>
2024-06-24 16:37:28 +0800

    commit

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ echo "Asia/Shanghai" > /etc/timezone
 
 mkdir -p /data/logs/BilibiliLiveMonitor
 
-java -jar /root/BiliBili_Danmuji.jar &
-nohup java -jar -Dspring.datasource.url="$DB_URL"  -Dspring.datasource.username="$DB_UESR" -Dspring.datasource.password="$DB_PASSWORD" /root/BilibiliLiveMonitor.jar > /data/logs/BilibiliLiveMonitor/info.log 2>&1 &
+nohup java -jar /root/BiliBili_Danmuji.jar > dev/null 2>&1 &
+nohup java -jar -Dspring.datasource.url="$DB_URL"  -Dspring.datasource.username="$DB_UESR" -Dspring.datasource.password="$DB_PASSWORD" /root/BilibiliLiveMonitor.jar > dev/null 2>&1 &
 
